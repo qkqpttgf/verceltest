b62d1dc8b1c0ed38491887aca18335e7e822f71e
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2021-08-07 20:53:37 +0800

    Update Onedrive.php

--- api/disk/Onedrive.php
+++ api/disk/Onedrive.php
@@ -352,7 +352,7 @@ class Onedrive {
         $result = $this->MSAPI('DELETE', $filename);
         //error_log1("Result: " . $result['body']);
         if ($result['stat']!=204) $r_body = json_encode($this->files_format(json_decode($result['body'], true)));
-        return output($, $result['stat']);
+        return output($r_body, $result['stat']);
         //return output($result['body'], $result['stat']);
     }
     public function Encrypt($folder, $passfilename, $pass) {
